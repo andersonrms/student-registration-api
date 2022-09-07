064759b181b0444bad7e3aa4f94c9e76f38e54dc
Anderson Ramos <andersonrms15@gmail.com>
2022-09-07 20:33:32 -0300

    feat: add tabela e rota de usuarios, validação de erros

---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import bcryptjs from 'bcryptjs';
+import { Model, Sequelize } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 256],
+            msg: 'Campo nome deve ter entre 3 e 256 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'Campo senha precisar ter entre 6 e 50 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
+    return this;
+  }
+}
